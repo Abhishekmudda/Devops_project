3c51ac75f25699b0ff36503e3ebd184e4e7616eb
Abhishek <abhimudda2003@gmail.com>
2025-11-18 14:56:11 +0530

    configure s3 has backend

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
-  # backend "s3" {
-  # bucket         = "abhishek-112233-bucket"
-  # key            = "eks/terraform.tfstate"   # path inside the bucket
-  # region         = "us-east-1"
-  # dynamodb_table = "terraform-lock-table"
-  # encrypt        = true
-  # }
+  backend "s3" {
+  bucket         = "abhishek-112233-bucket"
+  key            = "eks/terraform.tfstate"   # path inside the bucket
+  region         = "us-east-1"
+  dynamodb_table = "terraform-lock-table"
+  encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
