cbf235d7ad9857beba55ef07f722b0352513080e
Abhishek <abhimudda2003@gmail.com>
2025-11-19 14:53:58 +0530

    change file extension

--- Terraform/Ansible_output.tf
+++ Terraform/Ansible_output.tf
@@ -14,10 +14,10 @@
 # }
 
 resource "local_file" "ansible_yaml" {
-  content  = templatefile("${path.module}/output-values.yaml.tpl", {
+  content  = templatefile("${path.module}/output-values.aml.tpl", {
     vpc_id       = module.my_vpc.vpc_id
     cluster_name = aws_eks_cluster.my_cluster.name
     bastion_ip   = aws_instance.My_EC2_instance.public_ip
   })
-  filename = "./vars.yaml"
+  filename = "./vars.yml"
 }
