dcdee8e213da2f1000fa1ecd68658022b9f7c980
Abhishek <abhimudda2003@gmail.com>
2025-11-24 17:05:24 +0530

    add cluster

--- Terraform/Ansible_output.tf
+++ Terraform/Ansible_output.tf
@@ -1,8 +1,8 @@
-# resource "local_file" "ansible_yaml" {
-#   content = templatefile("${path.module}/output-values.yml.tpl", {
-#     vpc_id       = module.my_vpc.vpc_id
-#     cluster_name = aws_eks_cluster.my_cluster.name
-#     bastion_ip   = aws_instance.My_EC2_instance.public_ip
-#   })
-#   filename = "./vars.yml"
-# }
+resource "local_file" "ansible_yaml" {
+  content = templatefile("${path.module}/output-values.yml.tpl", {
+    vpc_id       = module.my_vpc.vpc_id
+    cluster_name = aws_eks_cluster.my_cluster.name
+    bastion_ip   = aws_instance.My_EC2_instance.public_ip
+  })
+  filename = "./vars.yml"
+}
