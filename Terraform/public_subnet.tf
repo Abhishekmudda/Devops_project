fd51bf577e5efd93ec3354c2f7418dc704c47aeb
Abhishek <abhimudda2003@gmail.com>
2025-11-24 15:10:23 +0530

    add module for subnet

--- Terraform/public_subnet.tf
+++ Terraform/public_subnet.tf
@@ -1,8 +1,7 @@
 module "public_subnet" {
-  source            = "./modules/subnet_modules"
+  source            = "./modules/public_subnet_modules"
   subnet_name       = var.public_subnet_name
   subnet_cidr_block = var.public_subnet_cidr
   vpc_id            = module.my_vpc.vpc_id
-  val = "1"
   depends_on = [module.my_vpc]
 }
