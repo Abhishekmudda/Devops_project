dc2cc43d0f751a84b8ec0f9986f69542f1e5b6a7
Abhishek <abhimudda2003@gmail.com>
2025-11-25 12:00:23 +0530

    changes in the app.py

--- App/app.py
+++ App/app.py
@@ -11,28 +11,48 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 load_dotenv()
+db = SQLAlchemy() ##added
+login_manager = LoginManager() #added
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev-secret-key')
     db_url = os.getenv('DATABASE_URL')
-    if db_url:
-        # Render & others might give postgres://; SQLAlchemy needs postgresql://
-        if db_url.startswith("postgres://"):
-            db_url = db_url.replace("postgres://", "postgresql://", 1)
-        app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    # if db_url:
+    #     # Render & others might give postgres://; SQLAlchemy needs postgresql://
+    #     if db_url.startswith("postgres://"):
+    #         db_url = db_url.replace("postgres://", "postgresql://", 1)
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    # else:
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #### added 
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    with app.app_context():
+        db.create_all()
+    ### added
+     
     return app
 
-app = create_app()
-db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
 
-login_manager = LoginManager(app)
-login_manager.login_view = "login"
+
+app = create_app()
+# db = SQLAlchemy(app)
+# with app.app_context():
+#     db.create_all()
+
+# login_manager = LoginManager(app)
+# login_manager.login_view = "login"
 
 BRANCHES = ["CS", "IT", "ME", "ECE", "EE", "CE"]
 
@@ -64,10 +84,6 @@ class Student(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-# Make sure all tables exist
-with app.app_context():
-    db.create_all()
 
 # --- Forms ---
 class SignupForm(FlaskForm):
